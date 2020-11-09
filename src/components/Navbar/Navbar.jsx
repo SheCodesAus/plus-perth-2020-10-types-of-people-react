c3cb2c36b1b3a5fcaf8092cbeed9cdf840d38f71
Sophie Phoon <sophie.j.phoon@gmail.com>
2020-11-09 13:59:56 +0800

    googles place api autocomplete added; categories autocomplete added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,78 +3,85 @@ import { Link, useHistory, useLocation } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
-  const [LoggedIn, setLoggedIn] = useState(false);
-  const history = useHistory();
-  const location = useLocation();
-  let username = window.localStorage.getItem("username");
-  const [isOrg, setIsOrg] = useState(false);
+    const [LoggedIn, setLoggedIn] = useState(false);
+    const history = useHistory();
+    const location = useLocation();
+    let username = window.localStorage.getItem("username");
+    const [isOrg, setIsOrg] = useState(false);
 
-  useEffect(() => {
-    const token = window.localStorage.getItem("token");
-    token != null ? setLoggedIn(true) : setLoggedIn(false);
-  }, [location]);
+    useEffect(() => {
+        const token = window.localStorage.getItem("token");
+        token != null ? setLoggedIn(true) : setLoggedIn(false);
+    }, [location]);
 
-  const logout = () => {
-    window.localStorage.clear();
-    history.push("/");
-  };
+    const logout = () => {
+        window.localStorage.clear();
+        history.push("/");
+    };
 
-  const reloadprofile = () => {
-    window.location.reload();
-  };
+    const reloadprofile = () => {
+        window.location.reload();
+    };
 
-  return (
-    <>
-      <div id="navbar">
-        <div id="navbar-logo-container">
-          <Link to="/">
-            <img
-              id="navbar-logo"
-              src={window.location.origin + "/binary_logo.png"}
-              alt="Binary"
-            />
-          </Link>
-        </div>
-        <div id="navbar-menu-items">
-          {!LoggedIn ? (
-            <>
-              <Link className="navbar-menu-item" to="/login">
-                Login
-              </Link>
-              <Link className="navbar-menu-item" to="/signup">
-                Sign up
-              </Link>
-            </>
-          ) : (
-            <>
-              <Link className="navbar-menu-item" to="/create-event">
-                Create Event
-              </Link>
-              {/* for testing purposes */}
-              {/* {isOrg ? (
+    return (
+        <>
+            <div id="navbar">
+                <div id="navbar-logo-container">
+                    <Link to="/">
+                        <img
+                            id="navbar-logo"
+                            src={window.location.origin + "/binary_logo.png"}
+                            alt="Binary"
+                        />
+                    </Link>
+                </div>
+                <div id="navbar-menu-items">
+                    {!LoggedIn ? (
+                        <>
+                            <Link className="navbar-menu-item" to="/login">
+                                Login
+                            </Link>
+                            <Link className="navbar-menu-item" to="/signup">
+                                Sign up
+                            </Link>
+                        </>
+                    ) : (
+                        <>
+                            <Link
+                                className="navbar-menu-item"
+                                to="/create-event"
+                            >
+                                Create Event
+                            </Link>
+                            {/* for testing purposes */}
+                            {/* {isOrg ? (
                 <>
                   <Link className="navbar-menu-item" to="/create-event">Create Event</Link>
                 </>
               ) : (
                 <p></p>
               )} */}
-              <Link
-                className="navbar-menu-item"
-                to={`/profile/${username}`}
-                onClick={reloadprofile}
-              >
-                {username}Profile
-              </Link>
-              <Link className="navbar-menu-item" to="/" onClick={logout}>
-                Logout
-              </Link>
-            </>
-          )}
-        </div>
-      </div>
-      <div className="separation-container"></div>
-    </>
-  );
+                            <Link
+                                className="navbar-menu-item"
+                                to={`/profile/${username}`}
+                                onClick={reloadprofile}
+                            >
+                                {username}
+                            </Link>
+                            <Link
+                                className="navbar-menu-item"
+                                to="/"
+                                onClick={logout}
+                            >
+                                Logout
+                            </Link>
+                        </>
+                    )}
+                </div>
+            </div>
+            <div className="separation-container"></div>
+        </>
+    );
 };
 
 export default Navbar;
