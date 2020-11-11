2df1ade4b3b575f5074d918e9c3b3ad7a4033b34
Sophie Phoon <sophie.j.phoon@gmail.com>
2020-11-12 00:31:10 +0800

    added location to edit mentor profile form; fixed bug in mentor profile card

--- src/components/MentorProfileCard/EditMentorProfileForm.jsx
+++ src/components/MentorProfileCard/EditMentorProfileForm.jsx
@@ -2,191 +2,252 @@ import { useHistory } from "react-router-dom";
 import React, { Fragment, useState, useEffect } from "react";
 import { Form } from "react-bootstrap";
 import { Typeahead } from "react-bootstrap-typeahead";
+import usePlacesAutocomplete, {
+    getGeocode,
+    getLatLng,
+} from "use-places-autocomplete";
 import "./MentorProfileCard.css";
 
 function EditProfileFrom(props) {
-  const history = useHistory();
-  const { userData, mentorDataProfile } = props;
-  let username = localStorage.username;
-  const [categoryData, setCategoryData] = useState([]);
-
-  useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_URL}events/categories/`)
-      .then((results) => {
-        return results.json();
-      })
-      .then((data) => {
-        setCategoryData(data);
-      });
-  }, []);
-
-  const category_option = categoryData.map((category) => category.category);
-
-  const [credentials, setCredentials] = useState({
-    username: "",
-    email: "",
-  });
-
-  const [public_profile, SetPublic_profile] = useState({
-    name: "",
-    bio: "",
-    skills: [],
-    location: "",
-  });
-
-  useEffect(() => {
-    setCredentials({
-      username: userData.username,
-      email: userData.email,
-    });
-    SetPublic_profile({
-      bio: mentorDataProfile === null ? " " : mentorDataProfile.bio,
-      name: mentorDataProfile === undefined ? " " : mentorDataProfile.name,
-      skills: mentorDataProfile === null ? " " : mentorDataProfile.skills,
-      // skills: mentorDataProfile.skills,
-      location:
-        mentorDataProfile === undefined ? " " : mentorDataProfile.location,
-    });
-  }, [userData, mentorDataProfile]);
-
-  const updateCat = (newCat) => {
-    SetPublic_profile({ ...public_profile, skills: newCat });
-  };
-
-  const handleChange = (e) => {
-    const { id, value } = e.target;
-    setCredentials((prevCredentials) => ({
-      ...prevCredentials,
-      [id]: value,
-    }));
-    SetPublic_profile((prevCredentials) => ({
-      ...prevCredentials,
-      [id]: value,
-    }));
-  };
-
-  const editData = async () => {
-    let token = window.localStorage.getItem("token");
+    const history = useHistory();
+    const { userData, mentorDataProfile } = props;
     let username = localStorage.username;
+    const [categoryData, setCategoryData] = useState([]);
 
-    const response1 = await fetch(
-      `${process.env.REACT_APP_API_URL}users/${username}/`,
-      {
-        method: "put",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Token ${token}`,
-        },
-        body: JSON.stringify(credentials),
-      }
+    // set data...[account]
+    const [credentials, setCredentials] = useState({
+        username: "",
+        email: "",
+    });
+
+    // ...[profile]
+    const [publicProfile, setPublicProfile] = useState({
+        name: "",
+        bio: "",
+        skills: [],
+    });
+
+    // ...[profile/location]
+    const [locationData, setLocationData] = useState({
+        latitude: null,
+        longitude: null,
+        location: "",
+    });
+
+    // on load, set categories to choose from
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URL}events/categories/`)
+            .then((results) => {
+                return results.json();
+            })
+            .then((data) => {
+                setCategoryData(data);
+            });
+    }, []);
+
+    const category_option = categoryData.map((category) => category.category);
+
+    // on load set current values in form
+    useEffect(() => {
+        setCredentials({
+            username: userData.username,
+            email: userData.email,
+        });
+        setPublicProfile({
+            bio: mentorDataProfile === null ? " " : mentorDataProfile.bio,
+            name:
+                mentorDataProfile === undefined ? " " : mentorDataProfile.name,
+            skills: mentorDataProfile === null ? " " : mentorDataProfile.skills,
+        });
+        setLocationData({
+            location:
+                mentorDataProfile != null ? mentorDataProfile.location : null,
+        });
+    }, [userData, mentorDataProfile]);
+
+    // Unpacking the data from usePlacesAutocomplete library
+    const {
+        ready,
+        value,
+        suggestions: { status, data },
+        setValue,
+        clearSuggestions,
+    } = usePlacesAutocomplete({
+        debounce: 300,
+    });
+
+    // Handling input for location
+    const handleInput = (e) => {
+        setValue(e.target.value);
+    };
+
+    // Setting state as the user selects a suggestion
+    const handleSelect = ({ description }) => () => {
+        setValue(description, false);
+        clearSuggestions();
+
+        // Get latitude and longitude via utility functions
+        getGeocode({ address: description })
+            .then((results) => getLatLng(results[0]))
+            .then(({ lat, lng }) => {
+                setLocationData({
+                    latitude: lat,
+                    longitude: lng,
+                    location: description,
+                });
+            })
+            .catch((error) => {
+                console.log("Error:", error);
+            });
+    };
+
+    // Rendering the suggestions from the api as input changes
+    const renderSuggestions = () =>
+        data.map((suggestion) => {
+            // Unpack suggestion
+            const {
+                id,
+                structured_formatting: { main_text, secondary_text },
+            } = suggestion;
+
+            // Render list of suggestions
+            return (
+                <li key={id} onClick={handleSelect(suggestion)}>
+                    <strong>{main_text}</strong> <small>{secondary_text}</small>
+                </li>
+            );
+        });
+
+    const updateCat = (newCat) => {
+        setPublicProfile({ ...publicProfile, skills: newCat });
+    };
+
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        setCredentials((prevCredentials) => ({
+            ...prevCredentials,
+            [id]: value,
+        }));
+        setPublicProfile((prevCredentials) => ({
+            ...prevCredentials,
+            [id]: value,
+        }));
+    };
+
+    const editData = async () => {
+        let token = window.localStorage.getItem("token");
+        let username = localStorage.username;
+
+        const response1 = await fetch(
+            `${process.env.REACT_APP_API_URL}users/${username}/`,
+            {
+                method: "put",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                    Authorization: `Token ${token}`,
+                },
+                body: JSON.stringify(credentials),
+            }
+        );
+        const response = await fetch(
+            `${process.env.REACT_APP_API_URL}users/mentor/${username}/profile/`,
+            {
+                method: "put",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Token ${token}`,
+                },
+                body: JSON.stringify({
+                    ...publicProfile,
+                    location: locationData.location,
+                    latitude: locationData.latitude,
+                    longitude: locationData.longitude,
+                }),
+            }
+        );
+        return response.json();
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Submit pressed");
+        if (credentials.username) {
+            editData().then((response) => {
+                console.log(response);
+                history.push(`/profile/${username}`);
+            });
+        }
+    };
+
+    return (
+        <form className="form">
+            <div className="form-item">
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    value={credentials.email}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="form-item">
+                <label htmlFor="name">Name:</label>
+                <input
+                    type="text"
+                    id="name"
+                    defaultValue={publicProfile.name}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="form-item">
+                <label htmlFor="bio">Bio:</label>
+                <input
+                    type="text"
+                    id="bio"
+                    defaultValue={publicProfile.bio}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="form-item">
+                <label htmlFor="location">Event location</label>
+                <input
+                    id="location"
+                    value={value}
+                    onChange={handleInput}
+                    disabled={!ready}
+                    placeholder="Where would you like to see events?"
+                />
+            </div>
+            {status === "OK" && (
+                <div className="form-item">
+                    <label></label>
+                    <ul className="location-options">{renderSuggestions()}</ul>
+                </div>
+            )}
+            <div className="form-item">
+                <Fragment>
+                    <div className="category-item-container">
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Skills</Form.Label>
+                            <Typeahead
+                                id="skills"
+                                labelKey="skills"
+                                multiple
+                                onChange={updateCat}
+                                options={category_option}
+                                selected={publicProfile.skills}
+                            />
+                        </Form.Group>
+                    </div>
+                </Fragment>
+            </div>
+
+            <button className="btn" type="submit" onClick={handleSubmit}>
+                Update Account
+            </button>
+        </form>
     );
-    const response = await fetch(
-      `${process.env.REACT_APP_API_URL}users/mentor/${username}/profile/`,
-      {
-        method: "put",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Token ${token}`,
-        },
-        body: JSON.stringify(public_profile),
-      }
-    );
-    return response.json();
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Submit pressed");
-    if (credentials.username) {
-      editData().then((response) => {
-        console.log(response);
-        // window.localStorage.setItem("username", credentials.username);
-        history.push(`/profile/${username}`);
-      });
-    }
-  };
-
-  return (
-    <form className="form">
-      {/* <div className="form-item">
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          value={credentials.username}
-          onChange={handleChange}
-        />
-      </div> */}
-      <div className="form-item">
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={credentials.email}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="form-item">
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          defaultValue={public_profile.name}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="form-item">
-        <label htmlFor="bio">Bio:</label>
-        <input
-          type="text"
-          id="bio"
-          defaultValue={public_profile.bio}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="form-item">
-        <label htmlFor="location">Location:</label>
-        <input
-          type="text"
-          id="location"
-          defaultValue={public_profile.location}
-          onChange={handleChange}
-        />
-      </div>
-      {/* <div className="form-item">
-        <label htmlFor="skills">Skills:</label>
-        <input
-          type="text"
-          id="categories"
-          value={public_profile.skills}
-          onChange={handleChange}
-        />
-      </div> */}
-      <div className="form-item">
-        <Fragment>
-          <div className="category-item-container">
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Skills</Form.Label>
-              <Typeahead
-                id="skills"
-                labelKey="skills"
-                // defaultValue={public_profile.skills}
-                multiple
-                onChange={updateCat}
-                options={category_option}
-                // placeholder="Choose categories..."
-                selected={public_profile.skills}
-              />
-            </Form.Group>
-          </div>
-        </Fragment>
-      </div>
-
-      <button className="btn" type="submit" onClick={handleSubmit}>
-        Update Account
-      </button>
-    </form>
-  );
 }
 
 export default EditProfileFrom;
--- src/components/MentorProfileCard/MentorProfileCard.jsx
+++ src/components/MentorProfileCard/MentorProfileCard.jsx
@@ -5,212 +5,170 @@ import retrieveIcons from "../../utilities/retrieveIcons.js";
 import "./MentorProfileCard.css";
 
 const MentorProfileCard = (props) => {
-  const [mentorDataProfile, setMentorDataProfile] = useState({});
-  let user = window.localStorage.getItem("username");
-  const [isBusy, setBusy] = useState(true);
+    const [mentorDataProfile, setMentorDataProfile] = useState({});
+    let user = window.localStorage.getItem("username");
+    const [isBusy, setBusy] = useState(true);
 
-  const [eventsAttended, setEventsAttended] = useState({});
+    const [eventsAttended, setEventsAttended] = useState({});
 
-  const fetchMentorEvents = async () => {
-    const response = await fetch(
-      `${process.env.REACT_APP_API_URL}events/${props.props.username}/mentor-attendance/`
-    );
-    if (response.ok) {
-      // console.log(response);
-      const data = await response.json();
-      if (data) {
-        setEventsAttended(data);
-        console.log(data);
-        setBusy(false);
-      }
-      return;
-    }
-    const data = await response.json();
-  };
+    const fetchMentorEvents = async () => {
+        const response = await fetch(
+            `${process.env.REACT_APP_API_URL}events/${props.props.username}/mentor-attendance/`
+        );
+        if (response.ok) {
+            // console.log(response);
+            const data = await response.json();
+            if (data) {
+                setEventsAttended(data);
+                console.log(data);
+                setBusy(false);
+            }
+            return;
+        }
+        const data = await response.json();
+    };
 
+    const fetchMentor = async () => {
+        const response = await fetch(
+            `${process.env.REACT_APP_API_URL}users/mentor/${props.props.username}/profile/`
+        );
+        if (response.ok) {
+            // console.log(response);
+            const data = await response.json();
+            if (data) {
+                setMentorDataProfile(data);
+                // console.log(data);
+                setBusy(false);
+            }
+            return;
+        }
+        const data = await response.json();
+    };
+    useEffect(() => {
+        fetchMentor();
+        fetchMentorEvents();
+    }, []);
 
-  const fetchMentor = async () => {
-    const response = await fetch(
-      `${process.env.REACT_APP_API_URL}users/mentor/${props.props.username}/profile/`
-    );
-    if (response.ok) {
-      // console.log(response);
-      const data = await response.json();
-      if (data) {
-        setMentorDataProfile(data);
-        // console.log(data);
-        setBusy(false);
-      }
-      return;
-    }
-    const data = await response.json();
-  };
-  useEffect(() => {
-    fetchMentor();
-    fetchMentorEvents();
-  }, []);
+    // useEffect(() => {
+    //   fetch(`${process.env.REACT_APP_API_URL}events`)
+    //     .then((results) => {
+    //       return results.json();
+    //     })
+    //     .then((data) => {
+    //       setEventList(data);
+    //     });
+    // }, []);
 
-  // useEffect(() => {
-  //   fetch(`${process.env.REACT_APP_API_URL}events`)
-  //     .then((results) => {
-  //       return results.json();
-  //     })
-  //     .then((data) => {
-  //       setEventList(data);
-  //     });
-  // }, []);
+    const mentor_profile = {
+        is_org: props.props.is_org,
+        username: props.props.username,
+        email: props.props.email,
+        name: mentorDataProfile.name,
+        bio: mentorDataProfile.bio,
+        image:
+            "https://cdn.pixabay.com/photo/2015/03/03/08/55/portrait-657116_960_720.jpg",
+        skills: mentorDataProfile.skills,
+        location: mentorDataProfile.location,
+    };
 
-  const mentor_profile = {
-    is_org: props.props.is_org,
-    username: props.props.username,
-    email: props.props.email,
-    name: mentorDataProfile.name,
-    bio: mentorDataProfile.bio,
-    image:
-      "https://cdn.pixabay.com/photo/2015/03/03/08/55/portrait-657116_960_720.jpg",
-    // mentorDataProfile.image,
-    skills: mentorDataProfile.skills,
-    location: mentorDataProfile.location,
-  };
-
-  //   const eventsMentoredAt = [
-  //     {
-  //       id: 1,
-  //       event_name: "SheCodes Python Workshop",
-  //       event_description:
-  //         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed deserunt nulla, excepturi cumque velit iure distinctio itaque, non ad pariatur quod enim praesentium provident incidunt voluptas odio laboriosam asperiores corrupti a odit, eaque dolores laborum sequi ipsa. Iusto distinctio velit sint consectetur maxime repudiandae nemo nostrum! Beatae facere delectus tempora.",
-  //       event_image:
-  //         "https://cdn.pixabay.com/photo/2015/01/08/18/24/programming-593312_960_720.jpg",
-  //       event_location: "Riff",
-  //       organiser: "She Codes",
-  //       category: "Python",
-  //       event_date: "Dec 12, 2019",
-  //     },
-  //     {
-  //       id: 2,
-  //       event_name: "SheCodes Junior JavaScript Workshop",
-  //       event_description:
-  //         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed deserunt nulla, excepturi cumque velit iure distinctio itaque, non ad pariatur quod enim praesentium provident incidunt voluptas odio laboriosam asperiores corrupti a odit, eaque dolores laborum sequi ipsa. Iusto distinctio velit sint consectetur maxime repudiandae nemo nostrum! Beatae facere delectus tempora.",
-  //       event_image:
-  //         "https://cdn.pixabay.com/photo/2015/01/08/18/24/children-593313_960_720.jpg",
-  //       event_location: "Flux",
-  //       organiser: "She Codes",
-  //       category: "JavaScript",
-  //       event_date: "Mar 20, 2019",
-  //     },
-  //     {
-  //       id: 3,
-  //       event_name: "SheCodes One-Day Workshop",
-  //       event_description:
-  //         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed deserunt nulla, excepturi cumque velit iure distinctio itaque, non ad pariatur quod enim praesentium provident incidunt voluptas odio laboriosam asperiores corrupti a odit, eaque dolores laborum sequi ipsa. Iusto distinctio velit sint consectetur maxime repudiandae nemo nostrum! Beatae facere delectus tempora.",
-  //       event_image:
-  //         "https://cdn.pixabay.com/photo/2017/01/12/10/40/school-1974369_960_720.jpg",
-  //       event_location: "Riff",
-  //       organiser: "She Codes",
-  //       category: "Python",
-  //       event_date: "Jan 9, 2019",
-  //     },
-  //   ];
-
-  // fetch events mentored at and replace state value here:
-  //   const [events, setEvents] = useState(eventsMentoredAt);
-  // const skillIcons = retrieveIcons(mentor_profile.skills).map((icon) => (
-  //   <>{icon}</>
-  // ));
-
-  function IsOwnerCanEdit() {
-    // if (username != null) {
-    if (user === mentor_profile.username) {
-      return (
-        <div id="owner-links">
-          <Link to={`/profile/${user}/edit`}>
-            <p>Edit</p>
-          </Link>
-          <Link to={`/profile/${user}/delete`}>
-            <p>Delete</p>
-          </Link>
-        </div>
-      );
-    } else {
-      return <p></p>;
-    }
-    // }
-  }
-  return (
-    <>
-      {isBusy ? (
-        <p>loading</p>
-      ) : (
-        <>
-          <div id="profile-exist">
-            {(mentorDataProfile.name === null ||
-              mentorDataProfile.name === undefined) &&
-            mentorDataProfile.skills.length == 0 &&
-            // (mentor_profile.image === null ||
-            //   mentor_profile.image === undefined) &&
-            (mentor_profile.bio === null ||
-              mentor_profile.bio === undefined) ? (
-              <div>
-                <h2>{mentor_profile.username}</h2>
-                <h2>There is no user profile set up for this user </h2>
-                {user === mentor_profile.username ? (
-                  <>
-                    <p>Tell us about yourself and your skills</p>
-                    <IsOwnerCanEdit />
-                    <br></br>
-                    <p>Email: {mentor_profile.email}</p>
-                  </>
-                ) : (
-                  <p></p>
-                )}
-              </div>
-            ) : (
-              <>
-                <div id="m-profile-sections-1-2">
-                  <div id="m-profile-section-1">
-                    <div id="m-profile-left">
-                      <img
-                        id="m-profile-image"
-                        src={mentor_profile.image}
-                        alt={mentor_profile.username}
-                      />
-                    </div>
-                    <div id="m-profile-right">
-                      <h1>{mentor_profile.name}</h1>
-                      <IsOwnerCanEdit />
-                      <h2>{mentor_profile.username}</h2>
-                      <p>Email: {mentor_profile.email}</p>
-                      {/* <div id="m-profile-skills-container">{skillIcons}</div> */}
-                      {retrieveIcons(mentor_profile.skills).map((icon) => (
-                        <>{icon}</>
-                      ))}
-
-                    </div>
-                  </div>
-                  <div id="m-profile-section-2">
-                    <h3>Bio</h3>
-                    <p>{mentor_profile.bio}</p>
-                    <p>{mentor_profile.location}</p>
-                  </div>
+    function IsOwnerCanEdit() {
+        if (user === mentor_profile.username) {
+            return (
+                <div id="owner-links">
+                    <Link to={`/profile/${user}/edit`}>
+                        <p>Edit</p>
+                    </Link>
+                    <Link to={`/profile/${user}/delete`}>
+                        <p>Delete</p>
+                    </Link>
                 </div>
-                <div id="m-profile-section-3">
-                  {/* <h3>Events I've signed up for</h3> */}
+            );
+        } else {
+            return <p></p>;
+        }
+        // }
+    }
+    return (
+        <>
+            {isBusy ? (
+                <p>loading</p>
+            ) : (
+                <>
+                    <div id="profile-exist">
+                        {(mentorDataProfile.name === null ||
+                            mentorDataProfile.name === undefined) &&
+                        mentorDataProfile.skills.length == 0 &&
+                        // (mentor_profile.image === null ||
+                        //   mentor_profile.image === undefined) &&
+                        (mentor_profile.bio === null ||
+                            mentor_profile.bio === undefined) ? (
+                            <div>
+                                <h2>{mentor_profile.username}</h2>
+                                <h2>
+                                    There is no user profile set up for this
+                                    user{" "}
+                                </h2>
+                                {user === mentor_profile.username ? (
+                                    <>
+                                        <p>
+                                            Tell us about yourself and your
+                                            skills
+                                        </p>
+                                        <IsOwnerCanEdit />
+                                        <br></br>
+                                        <p>Email: {mentor_profile.email}</p>
+                                    </>
+                                ) : (
+                                    <p></p>
+                                )}
+                            </div>
+                        ) : (
+                            <>
+                                <div id="m-profile-sections-1-2">
+                                    <div id="m-profile-section-1">
+                                        <div id="m-profile-left">
+                                            <img
+                                                id="m-profile-image"
+                                                src={mentor_profile.image}
+                                                alt={mentor_profile.username}
+                                            />
+                                        </div>
+                                        <div id="m-profile-right">
+                                            <h1>{mentor_profile.name}</h1>
+                                            <IsOwnerCanEdit />
+                                            <h2>{mentor_profile.username}</h2>
+                                            <p>Email: {mentor_profile.email}</p>
+                                            {/* <div id="m-profile-skills-container">{skillIcons}</div> */}
+                                            {retrieveIcons(
+                                                mentor_profile.skills
+                                            ).map((icon) => (
+                                                <>{icon}</>
+                                            ))}
+                                        </div>
+                                    </div>
+                                    <div id="m-profile-section-2">
+                                        <h3>Bio</h3>
+                                        <p>{mentor_profile.bio}</p>
+                                        <p>{mentor_profile.location}</p>
+                                    </div>
+                                </div>
+                                <div id="m-profile-section-3">
+                                    {/* <h3>Events I've signed up for</h3> */}
 
-                  <h3>Events I've mentored at</h3>
-                  {/* <div className="event-grid">
+                                    <h3>Events I've mentored at</h3>
+                                    {/* <div className="event-grid">
                     {eventsAttended.map((event) => {
                       return <EventCard event={event} />;
                     })}
                   </div> */}
-                </div>
-
-              </>
+                                </div>
+                            </>
+                        )}
+                    </div>
+                </>
             )}
-          </div>
         </>
-      )}
-    </>
-  );
+    );
 };
 
 export default MentorProfileCard;
