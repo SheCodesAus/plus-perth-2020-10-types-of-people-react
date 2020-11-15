bd9d6006a4a1e845376d6f62d06cb5bcc1da28cb
hellokirilee <hello.kiri.lee@gmail.com>
2020-11-15 20:55:30 +0800

    tidied up comments  - holding off on further validation

--- src/pages/PasswordPage.jsx
+++ src/pages/PasswordPage.jsx
@@ -31,60 +31,6 @@ const PasswordPage = () => {
   useEffect(() => {
     fetchPasswords();
   }, []);
-  // const onSubmit = async (passwordDetails, resetForm) => {
-  //   try {
-  //     console.log("--->", passwordDetails);
-  //     const token = window.localStorage.getItem("token");
-  //     console.log("token", token);
-  //     const response = await fetch(
-  //       `${process.env.REACT_APP_API_URL}users/${username}/update-password/`,
-  //       // "https://binary-api.herokuapp.com/users/org/update-password/",
-  //       {
-  //         method: "PUT",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //           Authorization: `Token ${token}`,
-  //         },
-  //         body: JSON.stringify({
-  //           old_password: passwordDetails.currentPassword,
-  //           new_password: passwordDetails.newPassword,
-  //         }),
-  //       }
-  //     );
-  //     // console.log(
-  //     //   JSON.stringify({
-  //     //     old_password: passwordDetails.currentPassword,
-  //     //     new_password: passwordDetails.newPassword,
-  //     //   })
-  //     // );
-  //     console.log("----1", response);
-  //     if (response.ok) {
-  //       alert("Password updated");
-  //       resetForm();
-  //     } else {
-  //       console.log(response.json());
-  //       alert("Could not update password");
-  //     }
-  //   } catch (err) {
-  //     console.log(err);
-  //     //   alert(`Network error: ${err.message}`);
-  //   }
-  // };
-  // const onSubmit = async () => {
-  //   let token = window.localStorage.getItem("token");
-  //   const response = await fetch(
-  //     `${process.env.REACT_APP_API_URL}users/${username}/update-password/`,
-  //     {
-  //       method: "put",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Authorization: `Token ${token}`,
-  //       },
-  //       body: JSON.stringify(credentials),
-  //     }
-  //   );
-  //   return response.json();
-  // };
 
   return (
     <div className="container">
@@ -98,15 +44,5 @@ const PasswordPage = () => {
       )}
     </div>
   );
-  // LoggedIn ? (
-  //   <div id="signup-page" className="container">
-  //     <>
-  //       <h1>Update Password</h1>
-  //       <PasswordForm Details={Details} />
-  //     </>
-  //   </div>
-  // ) : (
-  //   <Redirect to="/login" />
-  // );
 };
 export default PasswordPage;
