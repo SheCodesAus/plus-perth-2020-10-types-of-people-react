6d9b1d80bf14fe15853138f7953cedee13ad0692
lovesa <samara.love@bhp.com>
2020-11-15 10:03:41 +0800

    Password update working - some error statements included

--- src/components/PasswordForm/PasswordForm.jsx
+++ src/components/PasswordForm/PasswordForm.jsx
@@ -1,59 +1,148 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
-const PasswordForm = ({ onPasswordSubmit }) => {
+const PasswordForm = (props) => {
   // State
+  const { Details } = props;
+  let username = window.localStorage.getItem("username");
+  const history = useHistory();
+
   const [formDetails, setFormDetails] = useState({
-    currentPassword: "",
-    newPassword: "",
-    confirmNewPassword: "",
+    old_password: "",
+    new_password: "",
+    // confirmNewPassword: "",
   });
+  // console.log(Details);
+  const logout = () => {
+    window.localStorage.clear();
+    history.push("/login");
+  };
 
-  // Actions
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (formDetails.currentPassword === formDetails.newPassword) {
-      alert("New password can not be the same as the current one");
-      return;
-    }
-    if (formDetails.confirmNewPassword !== formDetails.newPassword) {
-      alert("Make sure new password and confirmed match");
-      return;
-    }
-    onPasswordSubmit(formDetails, () => {
-      setFormDetails({
-        currentPassword: "",
-        newPassword: "",
-        confirmNewPassword: "",
-      });
+  useEffect(() => {
+    setFormDetails({
+      old_password: Details.old_password,
+      new_password: Details.new_password,
     });
+  }, [Details]);
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormDetails((prevCredentials) => ({
+      ...prevCredentials,
+      [id]: value,
+    }));
   };
 
-  const onChange = (event) => {
-    setFormDetails({ ...formDetails, [event.target.name]: event.target.value });
+  const editData = async () => {
+    let token = window.localStorage.getItem("token");
+    const response = await fetch(
+      `${process.env.REACT_APP_API_URL}users/${username}/update-password/`,
+      {
+        method: "put",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Token ${token}`,
+        },
+        body: JSON.stringify(formDetails),
+      }
+    );
+    return response.json();
   };
+
+  //   <Link className="navbar-menu-item" to="/" onClick={logout}>
+  //   Logout
+  // </Link>
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submit pressed");
+
+    if (formDetails.new_password && formDetails.old_password) {
+      //   console.log("All data is there");
+      editData().then((response) => {
+        console.log(response);
+        console.log(response.code);
+        if (response.code === 200) {
+          alert("Password updated successfully");
+          {
+            logout();
+          }
+          // history.push("/login");
+        } else {
+          alert("Issue updating");
+          // alert("Password updated successfully. Login again");
+          // {
+          //   logout();
+          // }
+          // history.push("/login");
+        }
+
+        // // console.log("set local storage");
+        // history.push(`/`);
+      });
+    }
+    // alert("Current and new password need to be entered");
+  };
+  // Actions
+  // const onSubmit = (event) => {
+  //   event.preventDefault();
+  //   if (formDetails.currentPassword === formDetails.newPassword) {
+  //     alert("New password can not be the same as the current one");
+  //     return;
+  //   }
+  //   if (formDetails.confirmNewPassword !== formDetails.newPassword) {
+  //     alert("Make sure new password and confirmed match");
+  //     return;
+  //   }
+  //   onPasswordSubmit(formDetails, () => {
+  //     setFormDetails({
+  //       currentPassword: "",
+  //       newPassword: "",
+  //       confirmNewPassword: "",
+  //     });
+  //   });
+  // };
+
+  // const onChange = (event) => {
+  //   setFormDetails({ ...formDetails, [event.target.name]: event.target.value });
+  // };
 
   return (
-    <form onSubmit={onSubmit}>
-      <label htmlFor="currentPassword">Current Password</label>
-      <input
-        name="currentPassword"
-        value={formDetails.currentPassword}
-        onChange={onChange}
-      />
-      <label htmlFor="newPassword">New Password</label>
-      <input
-        name="newPassword"
-        value={formDetails.newPassword}
-        onChange={onChange}
-      />
-      <label htmlFor="confirmNewPassword">Confirm New Password</label>
+    <form className="form">
+      <div className="form-item">
+        <label htmlFor="old_password">Current Password</label>
+        <input
+          id="old_password"
+          type="password"
+          // value={formDetails.old_password}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="form-item">
+        <label htmlFor="new_password">New Password</label>
+        <input
+          id="new_password"
+          type="password"
+          // value={formDetails.new_password}
+          onChange={handleChange}
+        />
+      </div>
+      {/* <div className="form-item"></div> */}
+      {/* <label htmlFor="confirmNewPassword">Confirm New Password</label>
       <input
         name="confirmNewPassword"
         value={formDetails.confirmNewPassword}
-        onChange={onChange}
-      />
-      <button type="submit" name="update-password-button">
-        Submit
+               onChange={handleChange}
+
+      /> */}
+      {/* </div> */}
+      <button
+        type="submit"
+        // name="update-password-button"
+        onClick={handleSubmit}
+      >
+        Update Password
       </button>
     </form>
   );
--- src/pages/PasswordPage.jsx
+++ src/pages/PasswordPage.jsx
@@ -5,56 +5,108 @@ import PasswordForm from "../components/PasswordForm/PasswordForm";
 const PasswordPage = () => {
   const [LoggedIn, setLoggedIn] = useState(true);
   const location = useLocation();
+  let username = window.localStorage.getItem("username");
+  const [Details, setDetails] = useState({});
 
   useEffect(() => {
     const token = window.localStorage.getItem("token");
     token != null ? setLoggedIn(true) : setLoggedIn(false);
   }, [location]);
 
-  const onSubmit = async (passwordDetails, resetForm) => {
-    try {
-      console.log("--->", passwordDetails);
-      let username = window.localStorage.getItem("username");
-      const token = window.localStorage.getItem("token");
-      console.log("token", token);
-      const response = await fetch(
-        `${process.env.REACT_APP_API_URL}users/${username}/update-password/`,
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Token ${token}`,
-          },
-          data: JSON.stringify({
-            old_password: passwordDetails.currentPassword,
-            new_password: passwordDetails.newPassword,
-          }),
-        }
-      );
-      console.log("----1", response);
-      if (response.ok) {
-        alert("Password updated");
-        resetForm();
-      } else {
-        console.log(response.json());
-        alert("Could not update password");
+  const fetchPasswords = async () => {
+    let token = window.localStorage.getItem("token");
+    const response = await fetch(
+      `${process.env.REACT_APP_API_URL}users/${username}/update-password/`
+    );
+    if (response.ok) {
+      const data = await response.json();
+      if (data) {
+        setDetails(data);
       }
-    } catch (err) {
-      console.log(err);
-      //   alert(`Network error: ${err.message}`);
+      return;
     }
+    const data = await response.json();
   };
 
-  return LoggedIn ? (
-    <div id="signup-page" className="container">
-      <>
-        <h1>Update Password</h1>
-        <PasswordForm onPasswordSubmit={onSubmit} />
-      </>
-    </div>
-  ) : (
-    <Redirect to="/login" />
-  );
-};
+  useEffect(() => {
+    fetchPasswords();
+  }, []);
+  // const onSubmit = async (passwordDetails, resetForm) => {
+  //   try {
+  //     console.log("--->", passwordDetails);
+  //     const token = window.localStorage.getItem("token");
+  //     console.log("token", token);
+  //     const response = await fetch(
+  //       `${process.env.REACT_APP_API_URL}users/${username}/update-password/`,
+  //       // "https://binary-api.herokuapp.com/users/org/update-password/",
+  //       {
+  //         method: "PUT",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //           Authorization: `Token ${token}`,
+  //         },
+  //         body: JSON.stringify({
+  //           old_password: passwordDetails.currentPassword,
+  //           new_password: passwordDetails.newPassword,
+  //         }),
+  //       }
+  //     );
+  //     // console.log(
+  //     //   JSON.stringify({
+  //     //     old_password: passwordDetails.currentPassword,
+  //     //     new_password: passwordDetails.newPassword,
+  //     //   })
+  //     // );
+  //     console.log("----1", response);
+  //     if (response.ok) {
+  //       alert("Password updated");
+  //       resetForm();
+  //     } else {
+  //       console.log(response.json());
+  //       alert("Could not update password");
+  //     }
+  //   } catch (err) {
+  //     console.log(err);
+  //     //   alert(`Network error: ${err.message}`);
+  //   }
+  // };
+  // const onSubmit = async () => {
+  //   let token = window.localStorage.getItem("token");
+  //   const response = await fetch(
+  //     `${process.env.REACT_APP_API_URL}users/${username}/update-password/`,
+  //     {
+  //       method: "put",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Authorization: `Token ${token}`,
+  //       },
+  //       body: JSON.stringify(credentials),
+  //     }
+  //   );
+  //   return response.json();
+  // };
 
+  return (
+    <div className="container">
+      {LoggedIn ? (
+        <>
+          <h1>Update Password</h1>
+          <PasswordForm Details={Details} />
+        </>
+      ) : (
+        <Redirect to="/login" />
+      )}
+    </div>
+  );
+  // LoggedIn ? (
+  //   <div id="signup-page" className="container">
+  //     <>
+  //       <h1>Update Password</h1>
+  //       <PasswordForm Details={Details} />
+  //     </>
+  //   </div>
+  // ) : (
+  //   <Redirect to="/login" />
+  // );
+};
 export default PasswordPage;
