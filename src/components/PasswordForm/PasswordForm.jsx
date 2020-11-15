ddb6c2892d79c85addeca3224f957cdafde7fbbf
hellokirilee <hello.kiri.lee@gmail.com>
2020-11-15 20:54:52 +0800

    tidied up comments  - holding off on further validation

--- src/components/PasswordForm/PasswordForm.jsx
+++ src/components/PasswordForm/PasswordForm.jsx
@@ -1,4 +1,3 @@
-// import React, { useState } from "react";
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
@@ -11,9 +10,8 @@ const PasswordForm = (props) => {
   const [formDetails, setFormDetails] = useState({
     old_password: "",
     new_password: "",
-    // confirmNewPassword: "",
   });
-  // console.log(Details);
+
   const logout = () => {
     window.localStorage.clear();
     history.push("/login");
@@ -50,10 +48,6 @@ const PasswordForm = (props) => {
     return response.json();
   };
 
-  //   <Link className="navbar-menu-item" to="/" onClick={logout}>
-  //   Logout
-  // </Link>
-
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Submit pressed");
@@ -68,75 +62,28 @@ const PasswordForm = (props) => {
           {
             logout();
           }
-          // history.push("/login");
         } else {
           alert("Issue updating");
-          // alert("Password updated successfully. Login again");
-          // {
-          //   logout();
-          // }
-          // history.push("/login");
         }
-
-        // // console.log("set local storage");
-        // history.push(`/`);
       });
     }
-    // alert("Current and new password need to be entered");
   };
-  // Actions
-  // const onSubmit = (event) => {
-  //   event.preventDefault();
-  //   if (formDetails.currentPassword === formDetails.newPassword) {
-  //     alert("New password can not be the same as the current one");
-  //     return;
-  //   }
-  //   if (formDetails.confirmNewPassword !== formDetails.newPassword) {
-  //     alert("Make sure new password and confirmed match");
-  //     return;
-  //   }
-  //   onPasswordSubmit(formDetails, () => {
-  //     setFormDetails({
-  //       currentPassword: "",
-  //       newPassword: "",
-  //       confirmNewPassword: "",
-  //     });
-  //   });
-  // };
-
-  // const onChange = (event) => {
-  //   setFormDetails({ ...formDetails, [event.target.name]: event.target.value });
-  // };
 
   return (
     <form className="form">
       <div className="form-item">
         <label htmlFor="old_password">Current Password</label>
-        <input
-          id="old_password"
-          type="password"
-          // value={formDetails.old_password}
-          onChange={handleChange}
-        />
+        <input id="old_password" type="password" onChange={handleChange} />
       </div>
       <div className="form-item">
         <label htmlFor="new_password">New Password</label>
-        <input
-          id="new_password"
-          type="password"
-          // value={formDetails.new_password}
-          onChange={handleChange}
-        />
+        <input id="new_password" type="password" onChange={handleChange} />
       </div>
-      {/* <div className="form-item"></div> */}
-      {/* <label htmlFor="confirmNewPassword">Confirm New Password</label>
-      <input
-        name="confirmNewPassword"
-        value={formDetails.confirmNewPassword}
-               onChange={handleChange}
-
-      /> */}
-      {/* </div> */}
+      {/* placeholder for confirm new password validatoin */}
+      {/* <div className="form-item">
+        <label htmlFor="confirmNewPassword">Confirm New Password</label>
+        <input type="password" onChange={handleChange} />
+      </div> */}
       <button
         type="submit"
         // name="update-password-button"
